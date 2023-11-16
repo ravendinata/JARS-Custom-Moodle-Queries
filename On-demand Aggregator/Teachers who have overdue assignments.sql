d688c1d8d48c172889df0868f41a16e2b0f0d75b
Raven Limadinata <raven.limadinata@outlook.com>
2023-11-16 09:56:09 +0700

    Overdue assignment report
    Added queries for overdue assignment report. There are 3 variants:
    - Filterable using parameters
    - Statically assigned to the system role of teacher
    - Statically assigned to all student roles

---
+++ On-demand Aggregator/Teachers who have overdue assignments.sql
@@ -0,0 +1,28 @@
+SELECT DISTINCT
+CONCAT(u.firstname, " ", u.lastname) AS "Name",
+c.fullname AS "Course",
+a.name AS "Assignment",
+DATE_FORMAT(FROM_UNIXTIME(a.gradingduedate),'%Y-%m-%d %H:%i') AS "Due Date",
+DATEDIFF(NOW(), FROM_UNIXTIME(a.gradingduedate)) AS days_late
+
+FROM prefix_user_enrolments ue
+JOIN prefix_enrol AS e ON e.id = ue.enrolid
+JOIN prefix_course AS c ON c.id = e.courseid
+JOIN prefix_user AS u ON u.id = ue.userid
+JOIN prefix_assign a ON a.course = c.id
+JOIN prefix_role_assignments ra ON ra.userid = ue.userid
+JOIN prefix_role AS r ON r.id = ra.roleid
+
+/* Only show assignments that are due in the past 180 days */
+WHERE DATEDIFF(NOW(), FROM_UNIXTIME(a.gradingduedate)) > 0 
+AND DATEDIFF(NOW(), FROM_UNIXTIME(a.gradingduedate)) < 180
+AND ue.userid NOT IN 
+ (SELECT asub.userid
+  FROM prefix_assign_submission AS asub
+  JOIN prefix_assign AS a ON a.id = asub.assignment 
+  JOIN prefix_course c on a.course = c.id
+  WHERE asub.status = 'submitted')
+AND u.username != 'admin'
+AND r.shortname = 'teacher-syswide'
+
+ORDER BY days_late DESC, u.firstname, u.lastname, c.fullname, a.id, a.name, LENGTH(a.name)
