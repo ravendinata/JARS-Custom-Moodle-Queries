c26e88e0d54b0e9b38a56d5d612c69255dc80061
Raven Limadinata <raven.limadinata@outlook.com>
2023-12-13 10:27:45 +0700

    Update table naming in grade count view script

--- Views/Grade Count.sql
+++ Views/Grade Count.sql
@@ -1,9 +1,9 @@
 CREATE OR REPLACE VIEW grade_count AS
 SELECT gi.id AS itemid, COUNT(DISTINCT gg.userid) AS grade_count
 
-FROM mdl_grade_items AS gi
-JOIN mdl_grade_grades AS gg ON gg.itemid = gi.id
-JOIN mdl_course AS c ON c.id = gi.courseid
+FROM prefix_grade_items AS gi
+JOIN prefix_grade_grades AS gg ON gg.itemid = gi.id
+JOIN prefix_course AS c ON c.id = gi.courseid
 JOIN student_count AS sc ON sc.cid = c.id
 
 WHERE gi.itemname IS NOT NULL
